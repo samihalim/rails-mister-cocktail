a59f682b52fd71f5246d177e9e53cf56065163b6
Sami Halim <samihalim@gmail.com>
2020-11-19 21:06:29 +0100

    working controllers and models with banner and scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+result = JSON.parse(open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+drinks = result['drinks']
+drinks.each do |hash|
+  hash.each do |k, v|
+    Ingredient.create!(name: v)
+  end
+end
+lists = ['mojito', 'rum', 'pinacolda', 'vodka']
+lists.each do |cocktail|
+  Cocktail.create(
+    name: cocktail
+    )
+end
+puts "#{Cocktail.count} cocktails created"
+puts "#{Ingredient.count} ingredients created"
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
